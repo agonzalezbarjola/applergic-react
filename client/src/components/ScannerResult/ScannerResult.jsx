a5ca99fcddb16b96e7f7da4b9c8e34ccde7d1805
jean bravo <naejbravo@gmail.com>
2022-02-10 15:39:13 +0100

    compare allergys

--- client/src/components/ScannerResult/ScannerResult.jsx
+++ client/src/components/ScannerResult/ScannerResult.jsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 function ScannerResult({ props }) {
+  const product = props[0];
   const [user, setUser] = useState([]);
+  const userProduct = user.userDB;
+  console.log(userProduct);
+
+  const haveAllergy = [];
+
+  for (const item of userProduct.allergens) {
+    !product.allergens.includes(item) && haveAllergy.push(item) 
+    console.log(haveAllergy);
+  }
+
+
+
   const getUserById = async () => {
     const idStorage = JSON.parse(localStorage.getItem("id"));
     const res = await axios(`http://localhost:8000/api/users/${idStorage}`, {
@@ -29,18 +42,40 @@ function ScannerResult({ props }) {
         <button>x</button>
       </div>
       <div>
-        <h1>
-          Aqui tienes el resultado
-        </h1>
-        <p>{}</p>
-        
+        <h1>Aqui tienes el resultado</h1>
       </div>
-      {props.map((item) => (
+      <div>
+        <p>{haveAllergy.length}</p>
         <div>
-          {item.name}
-          <img src={item.image} alt=""></img>
+          <div>
+            <img src="" alt=""></img>
+            <img src={product.image} alt=""></img>
+          </div>
+
+          <div>
+            <img
+              src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644326247/favorito_3x_uuzvff.png"
+              alt=""
+            ></img>
+            <img
+              src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644326248/diario_3x_nsw6xi.png"
+              alt=""
+            ></img>
+            <img
+              src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644326247/red_3x_ivwedb.png"
+              alt=""
+            ></img>
+          </div>
         </div>
-      ))}
+        <div>
+          <h3>{product.name}</h3>
+          <h4>{product.brand}</h4>
+          <p>
+            <strong>Ingredientes:</strong> {product.ingredients}
+          </p>
+        </div>
+        <button>Escanea otro producto</button>
+      </div>
     </div>
   );
 }
