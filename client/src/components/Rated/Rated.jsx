22fa46ad3b4f84ee569ecdb98437525395dfb24f
jean bravo <naejbravo@gmail.com>
2022-02-10 15:07:49 +0100

    para alvaro

--- client/src/components/Rated/Rated.jsx
+++ client/src/components/Rated/Rated.jsx
@@ -1,51 +1,29 @@
-/* import React from 'react';
-import './Rated.scss';
-import ReactStars from "react-rating-stars-component";
+import { React, useState } from "react";
+import "./Rated.scss";
+import { Rating } from "react-simple-star-rating";
 
+export default function MyComponent() {
+  const [rating, setRating] = useState(0);
 
-const Rated = () => {
-
-  const secondExample = {
-    size: 50,
-    count: 10,
-    color: "black",
-    activeColor: "red",
-    value: 7.5,
-    a11y: true,
-    isHalf: true,
-    emptyIcon: <i className="far fa-star" />,
-    halfIcon: <i className="fa fa-star-half-alt" />,
-    filledIcon: <i className="fa fa-star" />,
-    onChange: newValue => {
-      console.log(`Example 2: new value is ${newValue}`);
-    }
+  const handleRating = (rate) => {
+    setRating(rate);
   };
-
-
-    
-
-
-
+  console.log(rating);
 
   return (
-      <div className="c-rated">
+    <div className="c-rated">
+      <div className="c-rated__hero">
+        <img
+          src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644261304/logoApplergic_laexqz.png"
+          alt=""
+        />
+        <h3>¡Gracias por usar Applergic!</h3>
 
-          <div className="c-rated__hero">
-          <img src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644261304/logoApplergic_laexqz.png" />
-          <h3>¡Gracias por usar Applergic!</h3>
-          <ReactStars {...secondExample} />
-
-          <h3>Por favor evalua tu experiencia.</h3>
-
-          </div>
-
-          <p>Enviar sugerencias</p>
-
-          
-
+        <Rating onClick={handleRating} ratingValue={rating} />
+        <h3>Por favor evalúa tu experiencia.</h3>
       </div>
-  )
-};
 
-export default Rated;
- */
+      <p>Enviar sugerencias</p>
+    </div>
+  );
+}
