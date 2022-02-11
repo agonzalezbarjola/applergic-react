706b508220ce1adced47426b3055287f3ef20c29
AndresVazqez <aavsz0811@gmail.com>
2022-02-11 20:36:47 +0100

    scanerpage cam styles & buttons

--- client/src/pages/ScannerPage/ScannerPage.jsx
+++ client/src/pages/ScannerPage/ScannerPage.jsx
@@ -8,6 +8,9 @@ function ScannerPage() {
   const [data, setData] = React.useState("Not found");
   const [text, setText] = React.useState("codigo de barras");
   const [loading, setLoading] = useState(true);
+  const [select, setSelect] = useState(true)
+
+
   useEffect(() => {
     const loadData = async () => {
       await new Promise((r) => setTimeout(r, 2000));
@@ -33,7 +36,7 @@ function ScannerPage() {
       {data === "Not found" ? (
         <div className="c-scannerPage">
           <div className="c-scannerPage__title">
-            <div className="navbar__close">
+            <div className="c-scannerPage__title--close">
               <img
                 src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
                 alt="exit"
@@ -49,6 +52,7 @@ function ScannerPage() {
           </div>
 
           <div className="c-scannerPage__scanner">
+
             <div className="c-scannerPage__scanner--cam">
               <BarcodeScannerComponent
                 width={300}
@@ -59,29 +63,31 @@ function ScannerPage() {
                 }}
               />
             </div>
+
             <div className="c-scannerPage__scanner--btn">
-              <div className="c-scannerPage__scanner--btn--item">
-                <img
-                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644326248/barcode2_3x_kjy5xg.png"
-                  alt=""
-                ></img>
+
+              <div onClick={() => setSelect(!select)} className="c-scannerPage__scanner--btn--item" >
+                {select == true ? (<img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644598851/oval_3x_xqzxrf.png" alt="barCode icon" />) :
+                  (<img
+                    src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644598840/barcode-negro_fbgvkc.png" />)}
                 <p>Codigo de barras</p>
               </div>
-              <div className="c-scannerPage__scanner--btn--item">
-                <img
-                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644489577/qr_3x_rnjgxu.png"
-                  alt=""
-                ></img>
+
+              <div onClick={() => setSelect(!select)} className="c-scannerPage__scanner--btn--item">
+                {select == false ? (<img src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644598841/qr-azul_nevak1.png" />) :
+                  (<img src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644598844/ovalCopy_3x_rosdoc.png" alt="qrCode icon" />)}
                 <p>Codigo QR</p>
               </div>
+
               <div className="c-scannerPage__scanner--btn--item">
                 <img
-                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644489507/nfc_yn7rro.png"
-                  alt=""
-                ></img>
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644489507/nfc_yn7rro.png" />
                 <p>NFC</p>
               </div>
+
             </div>
+
           </div>
         </div>
       ) : (
