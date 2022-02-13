3f6ec2a969a1231a4e139a19ea7d37ee0ae0348b
AndresVazqez <aavsz0811@gmail.com>
2022-02-13 20:41:38 +0100

    diary styles

--- client/src/components/DiaryList/DiaryList.jsx
+++ client/src/components/DiaryList/DiaryList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import qs from "qs";
+import "./DiaryList.scss";
 function DiaryList() {
   const diaryListArray = localStorage.getItem("code").split(",");
   //   console.log(diaryListArray);
@@ -49,29 +50,42 @@ function DiaryList() {
     console.log(product);
   }, []);
   return (
-    <div>
-      <div>
-        <img src="" alt="calanedar icon" />
-        <img src="" alt="filter " />
-        <img src="" alt="close" />
+    <div className="c-diaryList">
+      <div className="c-diaryList__icons">
+        <img
+          src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644775869/calendar_ughjqz.png"
+          alt="calanedar icon"
+        />
+        <img
+          src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644775899/filter_sfjtul.png"
+          alt="filter "
+        />
+        <img
+          src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
+          alt="close"
+        />
       </div>
-      <div>
-        <h3></h3>
-        <p></p>
+      <div className="c-diaryList__title">
+        <h3>¿Incluimos la selección en tu Diario?</h3>
+        <p>Añade tus comentarios para completar tu información.</p>
       </div>
       {product.map(
         (item) =>
           item.allergens.length === 0 && (
-            <div key={item._id}>
-              <div>
-                <img src={item.image} alt={item.name} />
+            <div className="c-diaryList__products" key={item._id}>
+              <div className="c-diaryList__products--img">
+                <img className="c-diaryList__products--img--frame"
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644522246/border-verde_lruwuu.png"
+                  alt="frame green"
+                />
+                <img className="c-diaryList__products--img--product" src={item.image} alt={item.name} />
               </div>
-              <div>
+              <div className="c-diaryList__products--text">
                 <p> </p>
                 <p>{item.name} sin datos de alergia</p>
                 <p>Nota: </p>
               </div>
-              <div>
+              <div className="c-diaryList__products--icon">
                 <img
                   src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
                   alt="icon close"
@@ -87,16 +101,20 @@ function DiaryList() {
       {product.map((item) =>
         item.allergens.map((item2) =>
           allergensStorage.includes(item2) ? (
-            <div key={item._id}>
-              <div>
-                <img src={item.image} alt={item.name} />
+            <div className="c-diaryList__products" key={item._id}>
+              <div className="c-diaryList__products--img">
+                <img className="c-diaryList__products--img--frame"
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644522246/border-rojo_prrt2l.png"
+                  alt="frame pink"
+                />
+                <img className="c-diaryList__products--img--product" src={item.image} alt={item.name} />
               </div>
-              <div>
+              <div className="c-diaryList__products--text">
                 <p> </p>
                 <p>{item.name} con alergia</p>
                 <p>Nota: </p>
               </div>
-              <div>
+              <div className="c-diaryList__products--icon">
                 <img
                   src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
                   alt="icon close"
@@ -108,16 +126,20 @@ function DiaryList() {
               </div>
             </div>
           ) : (
-            <div key={item._id}>
-              <div>
-                <img src={item.image} alt={item.name} />
+            <div className="c-diaryList__products green" key={item._id}>
+              <div className="c-diaryList__products--img">
+                <img className="c-diaryList__products--img--frame"
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644522246/border-verde_lruwuu.png"
+                  alt="frame green"
+                />
+                <img className="c-diaryList__products--img--product" src={item.image} alt={item.name} />
               </div>
-              <div>
+              <div className="c-diaryList__products--text">
                 <p> </p>
                 <p>{item.name} sin alergia</p>
                 <p>Nota: </p>
               </div>
-              <div>
+              <div className="c-diaryList__products--icon">
                 <img
                   src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
                   alt="icon close"
