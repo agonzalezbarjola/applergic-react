db8ef1c2f8b8e6df49ff410d7b374459f3f33ca0
jean bravo <naejbravo@gmail.com>
2022-02-13 18:54:43 +0100

    verify allergen done

--- client/src/components/DiaryList/DiaryList.jsx
+++ client/src/components/DiaryList/DiaryList.jsx
@@ -6,7 +6,7 @@ function DiaryList() {
   //   console.log(diaryListArray);
   const diaryFinal = [];
   const [product, setProduct] = useState([]);
-  const [ verify, setVerify ] = useState([]);
+  const [verify, setVerify] = useState([]);
   diaryListArray.forEach((item) => {
     //pushes only unique element
     if (!diaryFinal.includes(item)) {
@@ -14,37 +14,26 @@ function DiaryList() {
     }
   });
   const diaryList = diaryFinal.filter((item) => item !== "");
-  console.log(diaryList);
+  // console.log(diaryList);
 
   const allergensStorage = JSON.parse(localStorage.getItem("allergens"));
-  console.log(allergensStorage);
-
-  
-
+  // console.log(allergensStorage);
 
   const getProducts = async () => {
-    const res = await axios
-      .get(`http://localhost:8000/api/products/`, {
-        params: { codes: diaryList },
-        paramsSerializer: (params) => {
-          return qs.stringify(params, { arrayFormat: "repeat" });
-        },
-        headers: {
-          Authorization: {
-            toString() {
-              return `Bearer ${localStorage.getItem("token")}`;
-            },
+    const res = await axios.get(`http://localhost:8000/api/products/`, {
+      params: { codes: diaryList },
+      paramsSerializer: (params) => {
+        return qs.stringify(params, { arrayFormat: "repeat" });
+      },
+      headers: {
+        Authorization: {
+          toString() {
+            return `Bearer ${localStorage.getItem("token")}`;
           },
         },
-      })
+      },
+    });
     setProduct(res.data.res);
-    console.log(product)
-
-    
-
-    
-
-
 
     // .then((res) => {
     //   console.log(res);
@@ -55,18 +44,10 @@ function DiaryList() {
     // });
   };
 
-  product.map(item => allergensStorage.map(item2 => 
-    item.allergens.includes(item2) ? setVerify(true) : setVerify(false) ))
-
-    
   useEffect(() => {
     getProducts();
-    
-
+    console.log(product);
   }, []);
-
-  console.log(verify);
-
   return (
     <div>
       <div>
@@ -78,26 +59,78 @@ function DiaryList() {
         <h3></h3>
         <p></p>
       </div>
-      {product.map((item) =>
-
-
-
-
-        <div key={item._id} >
-
-          <div><img src={item.image} alt={item.name} /></div>
-          <div>
-            <p> </p>
-            <p>{item.name}</p>
-            <p>Nota:  </p>
-          </div>
-          <div>
-            <img src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png" alt="icon close" />
-            <img src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644753878/editicon_reuedr.png" alt="icon Edit" />
-          </div>
-        </div>
+      {product.map(
+        (item) =>
+          item.allergens.length === 0 && (
+            <div key={item._id}>
+              <div>
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div>
+                <p> </p>
+                <p>{item.name} sin datos de alergia</p>
+                <p>Nota: </p>
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
+                  alt="icon close"
+                />
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644753878/editicon_reuedr.png"
+                  alt="icon Edit"
+                />
+              </div>
+            </div>
+          )
       )}
-
+      {product.map((item) =>
+        item.allergens.map((item2) =>
+          allergensStorage.includes(item2) ? (
+            <div key={item._id}>
+              <div>
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div>
+                <p> </p>
+                <p>{item.name} con alergia</p>
+                <p>Nota: </p>
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
+                  alt="icon close"
+                />
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644753878/editicon_reuedr.png"
+                  alt="icon Edit"
+                />
+              </div>
+            </div>
+          ) : (
+            <div key={item._id}>
+              <div>
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div>
+                <p> </p>
+                <p>{item.name} sin alergia</p>
+                <p>Nota: </p>
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644327667/close_3x_qcn0b4.png"
+                  alt="icon close"
+                />
+                <img
+                  src="https://res.cloudinary.com/dkv0drgbb/image/upload/v1644753878/editicon_reuedr.png"
+                  alt="icon Edit"
+                />
+              </div>
+            </div>
+          )
+        )
+      )}
     </div>
   );
 }
