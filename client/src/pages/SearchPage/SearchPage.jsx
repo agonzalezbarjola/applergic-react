3186bc5e02daefa123ee2be5c6c8e2dbba36ae2f
unknown <agonzalezbarjola@gmail.com>
2022-02-15 19:41:24 +0100

    Navbar Footer SearchPage

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Search from "../../components/Search/Search"
 import Buscar from "../../components/Search/Busqueda"
+import Footer from "../../components/Footer/Footer"
 const SearchPage = () => {
   return (
     <div>
         <Search details ={Buscar}/>
+        <Footer/>
     </div>
   )
 }
